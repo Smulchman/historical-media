79041af7a0d233968ccd125125b0c026a2edcd99
Elvis Diaz <elvisdiaz020@gmail.com>
2023-03-22 12:13:42 -0400

    updates user profile GET route to get info for user profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,5 +12,22 @@ router.get("/", async (req, res) => {
 });
 
 // Dashboard/User profile GET "/user-profile/:id"
+router.get("/user-profile/:id", withAuth, async (req, res) => {
+  try {
+    const blogPostData = await blogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: Blog,
+          attributes: ["id", "title", "description"],
+        },
+      ],
+    });
+    const userProfile = blogPostData.get({ plain: true });
+    res.render("userProfile", { userProfile, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
