15b9854482f8f33d2963fdd98b870613be271de3
smulch <mulcahy.samuel@gmail.com>
2023-03-22 13:48:06 -0400

    capitalized class name in model extension line

--- models/event.js
+++ models/event.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class event extends Model {}
+class Event extends Model {}
 
 Event.init(
   {
