ca0c1fc95ccf786cbfd511f1aeabe754e19436d9
Elvis Diaz <elvisdiaz020@gmail.com>
2023-03-22 15:06:07 -0400

    fixes event.js to database json and fixes the naming of the models

--- models/event.js
+++ models/event.js
@@ -26,13 +26,14 @@ Event.init(
     },
     // 'wikipedia' value from API gives an array of objects, each object containing a wikipedia link and a simple title for that link. Likely to contain several links.
     wikipedia: {
-        type: DataTypes.ARRAY,
-        allowNull: false,
-      },
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     underscored: true,
+    freezeTableName: true,
     modelName: "event",
   }
 );
