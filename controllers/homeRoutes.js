7d8dfe0ad356dd002e862d99923e2d62bd333b29
Laert Xhumari <laertxhumari@gmail.com>
2023-03-22 12:55:32 -0400

    created a get route for the /all-events endpoint

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,6 +17,20 @@ router.get("/", async (req, res) => {
   //  });
   //   const posts = postData.map((post) => postData.get({ plain: true }));
   //   const randList = getRandElements(posts, 10);
+  //   res.render("home", { randList });
+});
+
+router.get("/all-events", async (req, res) => {
+  // const month = new Date(date).getMonth() + 1;
+  // const day = new Date(date).getDate();
+  //   const postData = await MODEL.findAll({
+  //   where: {
+  //     day: day,
+  //     month: month
+  //   }
+  //  });
+  //   const posts = postData.map((post) => postData.get({ plain: true }));
+  //   console.log(posts);
   //   res.render("home", { posts });
 });
 
