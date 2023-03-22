c9d1ef4573e6bddc91f106c0f8be3b0fa06c6a19
smulch <mulcahy.samuel@gmail.com>
2023-03-22 13:47:28 -0400

    set up event model

---
+++ models/event.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class event extends Model {}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    wikipedia: {
+        type: DataTypes.ARRAY,
+        allowNull: false,
+      },
+    blog_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'blog',
+          key: 'id',
+        },
+      },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: "event",
+  }
+);
+
+module.exports = Blog;
