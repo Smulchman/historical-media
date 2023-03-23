83e6c8bf288b023dc74f51690a870d7d8871db97
Elvis Diaz <elvisdiaz020@gmail.com>
2023-03-23 12:39:53 -0400

    adds username finder and checker in userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-// Create new user
+// Create new user, POST ("/api/")
 router.post("/", async (req, res) => {
   try {
     const dbUserdata = await User.create({
@@ -18,3 +18,25 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    // Email/Username Checker
+    const dbUserdata = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    // If no or wrong email/username, try again
+    if (!dbUserdata) {
+      res.status(400).json({ message: "Incorrect email. Please try again." });
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
