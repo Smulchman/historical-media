54546abe8c3c58f7f01dee199ec160383ca14d9c
Laert Xhumari <laertxhumari@gmail.com>
2023-03-23 14:11:25 -0400

    post blog completed

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,20 +1,16 @@
 const router = require("express").Router();
-const { User, Blog } = require("../../models");
+const { Blog } = require("../../models");
 
 // add a new blog POST("api/blog")
-// router.post("/", async (req, res) => {
-//   try {
-//     const dbUserdata = await User.create({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
-//     req.session.save(() => {
-//       req.session.loggedIn = true;
-//       res.status(200).json(dbUserdata);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const dbBlogData = await Blog.create({
+      title: req.body.title,
+      content: req.body.content,
+      event_id: req.body.event_id,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User } = require("../../models");
+const withAuth = require("../../utils/auth.js");
 
 // Create new user, POST ("/api/users")
 router.post("/", async (req, res) => {
