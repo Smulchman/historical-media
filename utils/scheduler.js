3f60a665ab38906c77a787cbe5868c0669bcb12f
Laert Xhumari <laertxhumari@gmail.com>
2023-03-24 12:46:43 -0400

    scheduler will now delete events that have no blogs associated with them

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -6,6 +6,31 @@ function scheduler() {
   cron.schedule("0 0 0 * * *", () => {
     const month = new Date().getMonth() + 1;
     const day = new Date().getDate();
+
+    //delete all events from the event table that have no blogs written about them
+    Event.findAll({
+      include: {
+        model: Blog,
+        required: true, // only return rows that have blogs written about them
+      },
+    })
+      .then((results) => {
+        const idsToSave = results.map((row) => row.id);
+        return ModelA.destroy({
+          where: {
+            id: {
+              [Sequelize.Op.notIn]: idsToSave, // only delete rows that do not have blogs written about them
+            },
+          },
+        });
+      })
+      .then((numRowsDeleted) => {
+        console.log(`Deleted ${numRowsDeleted} rows from Event.`);
+      })
+      .catch((err) => {
+        console.error("Error deleting rows:", err);
+      });
+
     console.log("Connected");
     fetch(`https://byabbe.se/on-this-day/${month}/${day}/events.json`)
       .then((response) => response.json())
