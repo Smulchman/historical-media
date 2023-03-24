5cfb6b463ee13ff9a6846cc0bc425a2fa63bfe69
Laert Xhumari <laertxhumari@gmail.com>
2023-03-24 16:45:29 -0400

    added some css for the no blogs found message

--- public/js/logout.js
+++ public/js/logout.js
@@ -12,3 +12,18 @@ const logout = async () => {
 };
 
 document.querySelector("#logout").addEventListener("click", logout);
+document.querySelector("#home").addEventListener("click", () => {
+  document.location.replace("/");
+});
+document.querySelector("#dashboard").addEventListener("click", () => {
+  document.location.replace("/api/blog/dashboard");
+});
+
+const blogButtons = document.querySelectorAll(".blog");
+
+for (let i = 0; i < blogButtons.length; i++) {
+  blogButtons[i].addEventListener("click", (event) => {
+    const event_id = event.target.getAttribute("data-eventId");
+    document.location.replace(`/blog/${event_id}`);
+  });
+}
