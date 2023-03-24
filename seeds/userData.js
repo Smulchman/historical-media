4b2f6fc70879f6005f95af4bacb87bc77e71b1b4
smulch <mulcahy.samuel@gmail.com>
2023-03-23 21:18:08 -0400

    encrypted seed passwords and added extra historical events to events table

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,25 +1,30 @@
 const { User } = require('../models');
 
 const userData = [
+  // I found an online generator to encrypt the passwords into bcrypt hashes. I used 10 rounds.
   {
     username: 'teeShirtLover',
     email: "loveTShirts@yahoo.net",
-    password: "tshirts4ever",
+    // original password: tshirts4ever
+    password: "$2a$10$4R4OCCkbAMY9nw82twXg.eIKPmWO26CdrZxpNxkJU0bRnCoVJNFky",
   },
   {
     username: 'smulchman',
     email: "mulcahy.samuel@gmail.com",
-    password: "D1ngusPatr0l",
+    // original password: D1ngusPatr0l
+    password: "$2a$10$.wTjnM8LCutXU0c4BIPao.O4lWIKRnrNY7QbgJ/i1e/u1vkpZW1eC",
   },
   {
     username: 'Elvisdiaz1',
     email: "elvisdiaz020@gmail.com",
-    password: "**CLICK**",
+    // original password: **CLICK**
+    password: "$2a$10$JubDlO6CVB71wArrQ.CQAOe3QN4rKDguU3U0dWgkJOXBD343H7kx6",
   },
   {
     username: 'JRob',
     email: "jroberts1931@gmail.com",
-    password: "ILoveMyFamily99",
+    // original password: ILoveMyFamily99
+    password: "$2a$10$q3Qx1ZzjF9RdetWDpyYX8ealE8khOPHjarzGdQdWWu4nv102MagoW",
   },
   {
     username: 'AlienOyi',
