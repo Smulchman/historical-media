24beec442e2199de7eb1ad9cbb514afa84987764
Laert Xhumari <laertxhumari@gmail.com>
2023-03-25 17:29:17 -0400

    delete button is now connected to the delete route

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,1 +1,23 @@
+const deleteButtons = document.querySelectorAll(".del-btn");
+for (let i = 0; i < deleteButtons.length; i++) {
+  deleteButtons[i].addEventListener("click", (event) => {
+    event.preventDefault();
+    const blog_id = event.target.getAttribute("data-blogId");
 
+    document
+      .querySelector(".btn-ok")
+      .addEventListener("click", async (event) => {
+        event.preventDefault();
+        const response = await fetch(`/blog/${blog_id}`, {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+          document.location.replace("/api/blog/dashboard");
+        } else {
+          alert("Failed To Delete.");
+        }
+      });
+  });
+}
